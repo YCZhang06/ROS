a1eabb40264d589044cfc92bace9ba10a8d1175d
yours <1542096471@qq.com>
2023-10-09 16:32:40 +0800

    1

--- communicate/include/communicate/communicate.h
+++ communicate/include/communicate/communicate.h
@@ -4,6 +4,7 @@
 #include "ros/ros.h"
 #include <std_msgs/String.h>
 #include "communicate/Person.h"
+#include "communicate/Multi.h"
 
 class communicate_template
 {
@@ -16,16 +17,20 @@ class communicate_template
 		ros::Subscriber normal_sub;
 		ros::Subscriber define_sub;
 
+		ros::ServiceServer server;
+		ros::ServiceClient client;
+
 		communicate_template(ros::NodeHandle nh);
 		~communicate_template();
 		void normal_pub_msg();
 		void define_pub_msg();
+		void request();
 		void run();
 
 	private:
 		void normal_callback(const std_msgs::String::ConstPtr &msg);  
 		void define_callback(const communicate::Person::ConstPtr &msg);  
-
+		bool response_callback(communicate::Multi::Request &req, communicate::Multi::Response &res);
 };
 
 #endif
--- communicate/src/topic.cpp
+++ communicate/src/topic.cpp
@@ -6,6 +6,8 @@ communicate_template::communicate_template(ros::NodeHandle handle)
 	normal_sub = handle.subscribe("Q",10,&communicate_template::normal_callback,this);
 	define_pub = handle.advertise<communicate::Person>("A", 10);
 	define_sub = handle.subscribe("A",10,&communicate_template::define_callback,this);
+	server = handle.advertiseService("multipy",&communicate_template::response_callback,this);
+	client = handle.serviceClient<communicate::Multi>("multipy");
 }
 
 communicate_template::~communicate_template() 
@@ -27,6 +29,19 @@ void communicate_template::define_pub_msg()
 	define_pub.publish(define_msg);
 }
 
+void communicate_template::request()
+{
+	ros::service::waitForService("multipy");
+	communicate::Multi m;
+	m.request.num1 = 6;
+    m.request.num2 = 6;
+	bool flag = client.call(m);
+	if (flag)
+    {
+        ROS_INFO("result: %ld",m.response.result);
+    }
+}
+
 void communicate_template::run()
 {
 	ros::Rate rate(1);     
@@ -41,10 +56,18 @@ void communicate_template::run()
 
 void communicate_template::normal_callback(const std_msgs::String::ConstPtr &msg)
 {
-	ROS_INFO("I know, you are: %s",msg->data.c_str());
+	ROS_INFO("question: %s",msg->data.c_str());
 }
 
 void communicate_template::define_callback(const communicate::Person::ConstPtr &msg)
 {
 	ROS_INFO("I know, you are: %s. age %d. height %.2f. ",msg->name.c_str(),msg->age,msg->height);
+}
+
+bool communicate_template::response_callback(communicate::Multi::Request &req,communicate::Multi::Response &res)
+{
+	int num1 = req.num1;
+    int num2 = req.num2;
+	res.result = num1 * num2;
+	return true;
 }
